b6dd6d2f6548a55cd35b21f63b99228176245419
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-04 15:38:44 +0200

    bullets added

--- Player.js
+++ Player.js
@@ -1,46 +1,66 @@
 export default class Player {
     rightPressed = false;
     leftPressed = false;
-
-    constructor(canvas, velocity) {
-        this.canvas = canvas;
-        this.velocity = velocity;
-        this.x = this.canvas.width/2;
-        this.y = this.canvas.height -75;
-        this.width = 50;
-        this.height = 50;
-        this.image = new Image();
-        this.image.src = "images/ship.png";
-
-        document.addEventListener("keydown", this.keydown);
-        document.addEventListener("keydown", this.keyup);
+    shootPressed = false;
+  
+    constructor(canvas, velocity, bulletController) {
+      this.canvas = canvas;
+      this.velocity = velocity;
+      this.bulletController = bulletController;
+  
+      this.x = this.canvas.width / 2;
+      this.y = this.canvas.height - 75;
+      this.width = 50;
+      this.height = 48;
+      this.image = new Image();
+      this.image.src = "images/ship.png";
+  
+      document.addEventListener("keydown", this.keydown);
+      document.addEventListener("keyup", this.keyup);
     }
     draw(ctx) {
-        this.move();
-        ctx.drawImage(this.image, this.x, this.y, this.height, this.width);
+      if (this.shootPressed) {
+        this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
+      }
+      this.move();
+      this.collideWithWalls();
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    collideWithWalls() {
+      if (this.x < 0) {
+        this.x = 0;
+        }
+        if (this.x > this.canvas.width - this.width) {
+        this.x = this.canvas.width - this.width;
+      }
     }
     move() {
-        if(this.rightPressed) {
-            this.x += this.velocity;
-        }
-        else if (this.leftPressed) {
-            this.x += -this.velocity;
-        } 
+      if (this.rightPressed) {
+        this.x += this.velocity;
+      } else if (this.leftPressed) {
+        this.x += -this.velocity;
+      }
     }
-    keydown = event => {
-        if(event.code === 'ArrowRight') {
-            this.rightPressed = true;
-        }
-        if(event.code === 'ArrowLeft') {
-            this.leftPressed = true;
-        }
-    }
-    keyup = event => {
-        if(event.code === 'ArrowRight') {
-            this.rightPressed = true;
-        }
-        if(event.code === 'ArrowLeft') {
-            this.leftPressed = true;
-        }
-    }
-}
+    keydown = (event) => {
+      if (event.code == "ArrowRight") {
+        this.rightPressed = true;
+      }
+      if (event.code == "ArrowLeft") {
+        this.leftPressed = true;
+      }
+      if (event.code == "Space") {
+        this.shootPressed = true;
+      }
+    };
+    keyup = (event) => {
+      if (event.code == "ArrowRight") {
+        this.rightPressed = false;
+      }
+      if (event.code == "ArrowLeft") {
+        this.leftPressed = false;
+      }
+      if (event.code == "Space") {
+        this.shootPressed = false;
+      }
+    };
+  }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import EnemyController from "./EnemyController.js";
 import Player from "./Player.js";
+import BulletController from "./BulletController.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -10,12 +11,16 @@ canvas.height = 600;
 const background = new Image();
 background.src = "images/space.png";
 
+const playerBulletController = new BulletController(canvas, 10, "red");
 const enemyController = new EnemyController(canvas);
-const player = new Player(canvas, 3);
+const player = new Player(canvas, 3, playerBulletController);
 
-function game () {
+function game() {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    enemyController.draw(ctx);
-    player.draw(ctx);
+      enemyController.draw(ctx);
+      player.draw(ctx);
+      playerBulletController.draw(ctx);
+      enemyBulletController.draw(ctx);
+    
 }
 setInterval(game, 1000/60);
