86405feb3f44b002904bdcef0f1982feb9202ce1
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-04 14:38:47 +0200

    enemies movement

--- EnemyController.js
+++ EnemyController.js
@@ -1,6 +1,95 @@
-export default class EnemyController {
-    constructor(canvas) {
-        this.canvas = 
-    }
+import Enemy from "./Enemy.js";
+import MovingDirection from "./MovingDirection.js";
 
+export default class EnemyController {
+    
+    enemyMap = [
+        [0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 0, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 0, 2, 2, 2, 2, 2, 2, 2, 0],
+    ];
+    enemyRows = [];
+    currentDirection = MovingDirection.right;
+    xVelocity = 0;
+    yVelocity = 0;
+    defaultXVelocity = 1;
+    defaultYVelocity = 1;
+    moveDownTimerDefault = 30;
+    moveDownTimer = this.moveDownTimerDefault;
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.createEnemies();
+    }
+    draw(ctx) {
+        this.decrementMoveDownTimer();
+        this.updateVelocityAndDirection();
+        this.drawEnemies(ctx);
+        this.resetMoveDownTimer();
+        console.log(this.moveDownTimer);
+    }
+    resetMoveDownTimer () {
+        if (this.moveDownTimer <= 0 ) {
+            this.moveDownTimer = this.moveDownTimerDefault;
+        }
+    }
+    decrementMoveDownTimer () {
+        if (
+            this.currentDirection === MovingDirection.downLeft ||
+            this.currentDirection === MovingDirection.downRight
+        ) {
+            this.moveDownTimer--;
+        }
+    }
+    updateVelocityAndDirection () {
+        for (const enemyRow of this.enemyRows) {
+            if(this.currentDirection == MovingDirection.right) {
+                this.xVelocity = this.defaultXVelocity;
+                this.yVelocity = 0;
+                const rightMosteEnemy = enemyRow[enemyRow.length -1];
+                if(rightMosteEnemy.x + rightMosteEnemy.width >= this.canvas.width) {
+                    this.currentDirection = MovingDirection.downLeft;
+                    break;
+                }
+            }
+            else if (this.currentDirection === MovingDirection.downLeft) {
+                this.xVelocity = 0;
+                this.yVelocity = this.defaultYVelocity;
+                if(this.moveDown(MovingDirection.left)) {
+                    break;
+                }
+            }
+        }
+    }
+    moveDown(newDirection) {
+        this.xVelocity = 0;
+        this.yVelocity = this.defaultYVelocity;
+        if (this.moveDownTimer <= 0) {
+            this.currentDirection = newDirection;
+            return true;
+        }
+        return false;
+    }
+    drawEnemies(ctx) {
+        this.enemyRows.flat().forEach((enemy) => {
+            enemy.move(this.xVelocity, this.yVelocity);
+            enemy.draw(ctx);
+        })
+    }
+    createEnemies () {
+        this.enemyMap.forEach((row, rowIndex) => {
+            this.enemyRows[rowIndex] = [];
+            row.forEach((enemyNumber, enemyIndex) => {
+                if (enemyNumber > 0) {
+                    this.enemyRows[rowIndex].push(new Enemy(enemyIndex * 60, rowIndex * 55, enemyNumber)
+
+                    )
+                }
+
+            })
+        })
+    }
 }
