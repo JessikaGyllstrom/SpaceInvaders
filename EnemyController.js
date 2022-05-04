66febdf5ddeca9dadaf21c54ba717400ccf47684
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-04 13:28:47 +0200

    project setup

---
+++ EnemyController.js
@@ -0,0 +1,6 @@
+export default class EnemyController {
+    constructor(canvas) {
+        this.canvas = 
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,19 @@
+import EnemyController from "./EnemyController.js";
+import EnemyController from "./EnemyController.js";
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+const background = new Image();
+background.src = "images/space.png";
+
+const EnemyController = new EnemyController(canvas);
+function game () {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    EnemyController.draw(ctx);
+    console.log("game");
+}
+setInterval(game, 1000/60);
